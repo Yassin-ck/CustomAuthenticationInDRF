54f61bdb2cab781428a0ae17ded5c063a7969210
Yassin-ck <mohammedyassinck@gmail.com>
2023-08-30 14:45:02 +0530

    first commit

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.StudentDetails.as_view(),name='student'),
+    path('<int:pk>/',views.StudentUpdates.as_view(),name='student'),
+]
